f62d7d328452193fc5be1eb19a4771f3039c9783
Nelson Dos Santos <katakicode001@gmail.com>
2026-01-19 09:29:10 +0100

    feat: add active pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { PhonePipe } from "./phone/phone-pipe";
 import { AddressPipe } from "./address/address-pipe";
+import { ActivePipe } from "./active/active-pipe";
 @NgModule({
   declarations: [],
-  imports: [PhonePipe, AddressPipe],
-  exports: [PhonePipe, AddressPipe],
+  imports: [PhonePipe, AddressPipe, ActivePipe],
+  exports: [PhonePipe, AddressPipe, ActivePipe],
 })
 export class PipesModule {}
