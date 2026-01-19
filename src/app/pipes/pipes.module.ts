41b6e3d1a21104daf7a7f96f0aa93590117c65d6
Nelson Dos Santos <katakicode001@gmail.com>
2026-01-19 09:54:38 +0100

    feat: add dash-if-empty pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,9 +3,10 @@ import { PhonePipe } from "./phone/phone-pipe";
 import { AddressPipe } from "./address/address-pipe";
 import { ActivePipe } from "./active/active-pipe";
 import { DatePipe } from "@angular/common";
+import { DashIfEmptyPipe } from "./dash-if-empty/dash-if-empty-pipe";
 @NgModule({
   declarations: [],
-  imports: [PhonePipe, AddressPipe, ActivePipe, DatePipe],
-  exports: [PhonePipe, AddressPipe, ActivePipe,DatePipe],
+  imports: [PhonePipe, AddressPipe, ActivePipe, DatePipe, DashIfEmptyPipe],
+  exports: [PhonePipe, AddressPipe, ActivePipe,DatePipe,DashIfEmptyPipe],
 })
 export class PipesModule {}
